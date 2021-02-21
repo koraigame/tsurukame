637cd006a8e0b29e1aa866f01c9742ced5cf60ae
UInt2048 <MBenedict2004@gmail.com>
2021-02-21 17:11:31 -0500

    Single implementation of settings, please

--- ios/Settings.swift
+++ ios/Settings.swift
@@ -12,216 +12,30 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import Foundation
-
-@objc enum ReviewOrder: UInt, Codable, CustomStringConvertible {
-  case random = 1
-  case ascendingSRSStage = 2
-  case currentLevelFirst = 3
-  case lowestLevelFirst = 4
-  case newestAvailableFirst = 5
-  case oldestAvailableFirst = 6
-  case descendingSRSStage = 7
-
-  var description: String {
-    switch self {
-    case .random: return "Random"
-    case .ascendingSRSStage: return "Ascending SRS stage"
-    case .descendingSRSStage: return "Descending SRS stage"
-    case .currentLevelFirst: return "Current level first"
-    case .lowestLevelFirst: return "Lowest level first"
-    case .newestAvailableFirst: return "Newest available first"
-    case .oldestAvailableFirst: return "Oldest available first"
-    }
-  }
-}
-
-@objc enum InterfaceStyle: UInt, Codable, CustomStringConvertible {
-  case system = 1
-  case light = 2
-  case dark = 3
-
-  var description: String {
-    switch self {
-    case .system: return "System"
-    case .light: return "Light"
-    case .dark: return "Dark"
-    }
-  }
-}
-
-private func setArchiveData<T: Codable>(_ object: T, key: String) {
-  var data: Data!
-  if #available(iOS 11.0, *) {
-    data = try! NSKeyedArchiver.archivedData(withRootObject: object, requiringSecureCoding: true)
-  } else {
-    let archiver = NSKeyedArchiver()
-    archiver.requiresSecureCoding = true
-    archiver.encode(object, forKey: key)
-    data = archiver.encodedData
-  }
-
-  UserDefaults.standard.setValue(data, forKey: key)
-}
-
-private func getArchiveData<T: Codable>(_ defaultValue: T, key: String) -> T {
-  // Encode anything not encoded
-  if let notEncodedObject = UserDefaults.standard.object(forKey: key) as? T {
-    setArchiveData(notEncodedObject, key: key)
-  }
-  // Decode value if obtainable and return it
-  guard let data = UserDefaults.standard.object(forKey: key) as? Data else {
-    setArchiveData(defaultValue, key: key)
-    return defaultValue
-  }
-  return (try? NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(data) as? T) ?? defaultValue
-}
-
-@propertyWrapper struct Setting<T: Codable> {
-  private let defaultValue: T
-  private let key: String
-
-  init(_ defaultValue: T, _ key: String) {
-    self.defaultValue = defaultValue
-    self.key = key
-  }
-
-  var wrappedValue: T {
-    get { getArchiveData(defaultValue, key: key) }
-    set(newValue) { setArchiveData(newValue, key: key) }
-  }
-}
-
-@propertyWrapper struct EnumSetting<T: RawRepresentable> where T.RawValue: Codable {
-  private let defaultValue: T
-  private let key: String
-
-  init(_ defaultValue: T, _ key: String) {
-    self.defaultValue = defaultValue
-    self.key = key
-  }
-
-  var wrappedValue: T {
-    get { T(rawValue: getArchiveData(defaultValue.rawValue, key: key))! }
-    set(newValue) { setArchiveData(newValue.rawValue, key: key) }
-  }
-}
-
-@propertyWrapper struct EnumArraySetting<A: Sequence, T: RawRepresentable> where A.Element == T,
-  T.RawValue: Codable {
-  private let defaultValue: [T.RawValue]
-  private let key: String
-
-  init(_ defaultValues: A, _ key: String) {
-    defaultValue = EnumArraySetting<A, T>.toRawArray(defaultValues)
-    self.key = key
-  }
-
-  private static func toRawArray(_ values: A) -> [T.RawValue] {
-    var ret = [T.RawValue]()
-    for value in values {
-      ret.append(value.rawValue)
-    }
-    return ret
-  }
-
-  private static func fromRawArray(_ values: [T.RawValue]) -> A {
-    var ret = [T]()
-    for value in values {
-      ret.append(T(rawValue: value)!)
-    }
-    return ret as! A
-  }
-
-  var wrappedValue: A {
-    get { EnumArraySetting<A, T>.fromRawArray(getArchiveData(defaultValue, key: key)) }
-    set(newValue) { setArchiveData(EnumArraySetting<A, T>.toRawArray(newValue), key: key) }
-  }
-}
-
-@objcMembers class Settings: NSObject {
-  @Setting("", #keyPath(userCookie)) static var userCookie: String
-  @Setting("", #keyPath(userEmailAddress)) static var userEmailAddress: String
-  @Setting("", #keyPath(userApiToken)) static var userApiToken: String
-
-  @EnumSetting(InterfaceStyle.system,
-               #keyPath(interfaceStyle)) static var interfaceStyle: InterfaceStyle
-
-  @Setting(false, #keyPath(notificationsAllReviews)) static var notificationsAllReviews: Bool
-  @Setting(true, #keyPath(notificationsBadging)) static var notificationsBadging: Bool
-
-  @Setting(false, #keyPath(prioritizeCurrentLevel)) static var prioritizeCurrentLevel: Bool
-  @EnumArraySetting([
-    .radical,
-    .kanji,
-    .vocabulary,
-  ], "lessonOrder") static var lessonOrder: [TKMSubject.TypeEnum]
-  @Setting(5, #keyPath(lessonBatchSize)) static var lessonBatchSize: Int
-
-  @EnumSetting(ReviewOrder.random, #keyPath(reviewOrder)) static var reviewOrder: ReviewOrder
-  @Setting(5, #keyPath(reviewBatchSize)) static var reviewBatchSize: Int
-  @Setting(false, #keyPath(groupMeaningReading)) static var groupMeaningReading: Bool
-  @Setting(true, #keyPath(meaningFirst)) static var meaningFirst: Bool
-  @Setting(true, #keyPath(showAnswerImmediately)) static var showAnswerImmediately: Bool
-  @Setting([], #keyPath(selectedFonts)) static var selectedFonts: Set<String>
-  @Setting(1.0, #keyPath(fontSize)) static var fontSize: Float
-  @Setting(false, #keyPath(exactMatch)) static var exactMatch: Bool
-  @Setting(true, #keyPath(enableCheats)) static var enableCheats: Bool
-  @Setting(true, #keyPath(showOldMnemonic)) static var showOldMnemonic: Bool
-  @Setting(true, #keyPath(useKatakanaForOnyomi)) static var useKatakanaForOnyomi: Bool
-  @Setting(false, #keyPath(showSRSLevelIndicator)) static var showSRSLevelIndicator: Bool
-  @Setting(false, #keyPath(showAllReadings)) static var showAllReadings: Bool
-  @Setting(false, #keyPath(autoSwitchKeyboard)) static var autoSwitchKeyboard: Bool
-  @Setting(false, #keyPath(allowSkippingReviews)) static var allowSkippingReviews: Bool
-  @Setting(true, #keyPath(minimizeReviewPenalty)) static var minimizeReviewPenalty: Bool
-
-  @Setting(false, #keyPath(playAudioAutomatically)) static var playAudioAutomatically: Bool
-  @Setting([], #keyPath(installedAudioPackages)) static var installedAudioPackages: Set<String>
-
-  @Setting(true, #keyPath(animateParticleExplosion)) static var animateParticleExplosion: Bool
-  @Setting(true, #keyPath(animateLevelUpPopup)) static var animateLevelUpPopup: Bool
-  @Setting(true, #keyPath(animatePlusOne)) static var animatePlusOne: Bool
-
-  @Setting(true,
-           #keyPath(subjectCatalogueViewShowAnswers)) static var subjectCatalogueViewShowAnswers: Bool
-}
-
-// Copyright 2021 David Sansome
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 
 struct S<T: Codable> {
-  static func archiveData(_ object: T, _ key: String) -> Data {
+  static func set(_ object: T, _ key: String) {
+    var data: Data!
     if #available(iOS 11.0, *) {
-      return try! NSKeyedArchiver.archivedData(withRootObject: object, requiringSecureCoding: true)
+      data = try! NSKeyedArchiver.archivedData(withRootObject: object, requiringSecureCoding: true)
     } else {
-      let data = NSMutableData()
-      let archiver = NSKeyedArchiver(forWritingWith: data)
+      let _data = NSMutableData()
+      let archiver = NSKeyedArchiver(forWritingWith: _data)
       archiver.requiresSecureCoding = true
       archiver.encode(object, forKey: key)
-      return data as Data
+      data = _data as Data
     }
+    UserDefaults.standard.set(data, forKey: key)
   }
   
   static func get(_ defaultValue: T, _ key: String) -> T {
     // Encode anything not encoded
     if let notEncodedObject = UserDefaults.standard.object(forKey: key) as? T {
-      UserDefaults.standard.set(archiveData(notEncodedObject, key), forKey: key)
+      set(notEncodedObject, key: key)
     }
     // Decode value if obtainable and return it
     guard let data = UserDefaults.standard.object(forKey: key) as? Data else {
-      UserDefaults.standard.set(archiveData(defaultValue, key), forKey: key)
+      set(defaultValue, key: key)
       return defaultValue
     }
     if #available(iOS 9.0, *) {
@@ -231,142 +45,162 @@ struct S<T: Codable> {
       return (NSKeyedUnarchiver.unarchiveObject(with: data) as? T) ?? defaultValue
     }
   }
-  
-  static func set(_ key: String, _ newValue: T) {
-    UserDefaults.standard.set(archiveData(newValue, key), forKey: key)
-  }
 }
 
-struct E<T: RawRepresentable> {}
-struct A<C: Sequence, T: RawRepresentable> {}
+struct E<T: RawRepresentable> where T.RawValue: Codable {
+	static func set(_ object: T, _ key: String) {
+		S.set(object.rawValue, key: key)
+	}
+	static func get(_ defaultValue: T, _ key: String) -> T {
+		return T(rawValue: S.get(defaultValue.rawValue, key: key))!
+	}
+}
+struct A<C: Sequence, T: RawRepresentable> where A.Element == T, T.RawValue: Codable {
+	private static func fromRawArray(_ values: [T.RawValue]) -> A {
+		var ret = [T]()
+    for value in values {
+      ret.append(T(rawValue: value)!)
+    }
+    return ret as! A
+	}
+	private static func toRawArray(_ values: A) -> [T,RawValue] {
+		var ret = [T.RawValue]()
+    for value in values {
+      ret.append(value.rawValue)
+    }
+    return ret
+	}
+	static func set(_ object: T, _key: String) {
+		S.set(toRawArray(object), key: key)
+	}
+	static func get(_ defaultValue: T, _ key: String) -> T {
+		return fromRawArray(S.get(defaultValue, key: key))
+	}
+}
 
 @objcMembers class Settings: NSObject {
   static var userCookie: String {
     get {return S.get("", #keyPath(userCookie))}
-    set(n) {S.set(#keyPath(userCookie), n)}
+    set(n) {S.set(n, #keyPath(userCookie))}
   }
   static var userEmailAddress: String {
     get {return S.get("", #keyPath(userEmailAddress))}
-    set(n) {S.set(#keyPath(userEmailAddress), n)}
+    set(n) {S.set(n, #keyPath(userEmailAddress))}
   }
   static var userApiToken: String {
     get {return S.get("", #keyPath(userApiToken))}
-    set(n) {S.set(#keyPath(userApiToken), n)}
+    set(n) {S.set(n, #keyPath(userApiToken))}
   }
-  static var interfaceStyle: UInt {
-    get {return S.get(InterfaceStyle.system.rawValue, #keyPath(interfaceStyle))}
-    set(n) {S.set(#keyPath(interfaceStyle), n)}
+  static var interfaceStyle: InterfaceStyle {
+    get {return E.get(InterfaceStyle.system, #keyPath(interfaceStyle))}
+    set(n) {E.set(n, #keyPath(interfaceStyle))}
   }
   static var notificationsAllReviews: Bool {
     get {return S.get(false, #keyPath(notificationsAllReviews))}
-    set(n) {S.set(#keyPath(notificationsAllReviews), n)}
+    set(n) {S.set(n, #keyPath(notificationsAllReviews))}
   }
   static var notificationsBadging: Bool {
     get {return S.get(false, #keyPath(notificationsAllReviews))}
-    set(n) {S.set(#keyPath(notificationsBadging), n)}
+    set(n) {S.set(n, #keyPath(notificationsBadging))}
   }
   static var prioritizeCurrentLevel: Bool {
     get {return S.get(false, #keyPath(prioritizeCurrentLevel))}
-    set(n) {S.set(#keyPath(prioritizeCurrentLevel), n)}
+    set(n) {S.set(n, #keyPath(prioritizeCurrentLevel))}
   }
-  static var lessonOrder: [Int32] {
-    get {return S.get([
-      TKMSubject_Type.radical.rawValue,
-      TKMSubject_Type.kanji.rawValue,
-      TKMSubject_Type.vocabulary.rawValue,
-    ], #keyPath(lessonOrder))}
-    set(n) {S.set(#keyPath(lessonOrder), n)}
+  static var lessonOrder: [TKMSubject.TypeEnum] {
+    get {return A.get([.radical, .kanji, .vocabulary], #keyPath(lessonOrder))}
+    set(n) {A.set(n, #keyPath(lessonOrder))}
   }
   static var lessonBatchSize: Int {
     get {return S.get(5, #keyPath(lessonBatchSize))}
-    set(n) {S.set(#keyPath(lessonBatchSize), n)}
+    set(n) {S.set(n, #keyPath(lessonBatchSize))}
   }
-  static var reviewOrder: UInt {
-    get {return S.get(ReviewOrder.random.rawValue, #keyPath(reviewOrder))}
-    set(n) {S.set(#keyPath(reviewOrder), n)}
+  static var reviewOrder: ReviewOrder {
+    get {return E.get(ReviewOrder.random, #keyPath(reviewOrder))}
+    set(n) {E.set(n, #keyPath(reviewOrder))}
   }
   static var reviewBatchSize: Int {
     get {return S.get(5, #keyPath(reviewBatchSize))}
-    set(n) {S.set(#keyPath(reviewBatchSize), n)}
+    set(n) {S.set(n, #keyPath(reviewBatchSize))}
   }
   static var groupMeaningReading: Bool {
     get {return S.get(false, #keyPath(groupMeaningReading))}
-    set(n) {S.set(#keyPath(groupMeaningReading), n)}
+    set(n) {S.set(n, #keyPath(groupMeaningReading))}
   }
   static var meaningFirst: Bool {
     get {return S.get(true, #keyPath(meaningFirst))}
-    set(n) {S.set(#keyPath(meaningFirst), n)}
+    set(n) {S.set(n, #keyPath(meaningFirst))}
   }
   static var showAnswerImmediately: Bool {
     get {return S.get(true, #keyPath(showAnswerImmediately))}
-    set(n) {S.set(#keyPath(showAnswerImmediately), n)}
+    set(n) {S.set(n, #keyPath(showAnswerImmediately))}
   }
   static var selectedFonts: Set<String> {
     get {return S.get([], #keyPath(selectedFonts))}
-    set(n) {S.set(#keyPath(selectedFonts), n)}
+    set(n) {S.set(n, #keyPath(selectedFonts))}
   }
   static var fontSize: Float {
     get {return S.get(1.0, #keyPath(fontSize))}
-    set(n) {S.set(#keyPath(fontSize), n)}
+    set(n) {S.set(n, #keyPath(fontSize))}
   }
   static var exactMatch: Bool {
     get {return S.get(false, #keyPath(exactMatch))}
-    set(n) {S.set(#keyPath(exactMatch), n)}
+    set(n) {S.set(n, #keyPath(exactMatch))}
   }
   static var enableCheats: Bool {
     get {return S.get(true, #keyPath(enableCheats))}
-    set(n) {S.set(#keyPath(exactMatch), n)}
+    set(n) {S.set(n, #keyPath(exactMatch))}
   }
   static var showOldMnemonic: Bool {
     get {return S.get(true, #keyPath(showOldMnemonic))}
-    set(n) {S.set(#keyPath(showOldMnemonic), n)}
+    set(n) {S.set(n, #keyPath(showOldMnemonic))}
   }
   static var useKatakanaForOnyomi: Bool {
     get {return S.get(true, #keyPath(useKatakanaForOnyomi))}
-    set(n) {S.set(#keyPath(useKatakanaForOnyomi), n)}
+    set(n) {S.set(n, #keyPath(useKatakanaForOnyomi))}
   }
   static var showSRSLevelIndicator: Bool {
     get {return S.get(false, #keyPath(showSRSLevelIndicator))}
-    set(n) {S.set(#keyPath(showSRSLevelIndicator), n)}
+    set(n) {S.set(n, #keyPath(showSRSLevelIndicator))}
   }
   static var showAllReadings: Bool {
     get {return S.get(false, #keyPath(showAllReadings))}
-    set(n) {S.set(#keyPath(showAllReadings), n)}
+    set(n) {S.set(n, #keyPath(showAllReadings))}
   }
   static var autoSwitchKeyboard: Bool {
     get {return S.get(false, #keyPath(autoSwitchKeyboard))}
-    set(n) {S.set(#keyPath(autoSwitchKeyboard), n)}
+    set(n) {S.set(n, #keyPath(autoSwitchKeyboard))}
   }
   static var allowSkippingReviews: Bool {
     get {return S.get(false, #keyPath(allowSkippingReviews))}
-    set(n) {S.set(#keyPath(allowSkippingReviews), n)}
+    set(n) {S.set(n, #keyPath(allowSkippingReviews))}
   }
   static var minimizeReviewPenalty: Bool {
     get {return S.get(true, #keyPath(minimizeReviewPenalty))}
-    set(n) {S.set(#keyPath(minimizeReviewPenalty), n)}
+    set(n) {S.set(n, #keyPath(minimizeReviewPenalty))}
   }
   static var playAudioAutomatically: Bool {
     get {return S.get(false, #keyPath(playAudioAutomatically))}
-    set(n) {S.set(#keyPath(playAudioAutomatically), n)}
+    set(n) {S.set(n, #keyPath(playAudioAutomatically))}
   }
   static var installedAudioPackages: Set<String> {
     get {return S.get([], #keyPath(installedAudioPackages))}
-    set(n) {S.set(#keyPath(installedAudioPackages), n)}
+    set(n) {S.set(n, #keyPath(installedAudioPackages))}
   }
   static var animateParticleExplosion: Bool {
     get {return S.get(true, #keyPath(animateParticleExplosion))}
-    set(n) {S.set(#keyPath(animateParticleExplosion), n)}
+    set(n) {S.set(n, #keyPath(animateParticleExplosion))}
   }
   static var animateLevelUpPopup: Bool {
     get {return S.get(true, #keyPath(animateLevelUpPopup))}
-    set(n) {S.set(#keyPath(animateLevelUpPopup), n)}
+    set(n) {S.set(n, #keyPath(animateLevelUpPopup))}
   }
   static var animatePlusOne: Bool {
     get {return S.get(true, #keyPath(animatePlusOne))}
-    set(n) {S.set(#keyPath(animatePlusOne), n)}
+    set(n) {S.set(n, #keyPath(animatePlusOne))}
   }
   static var subjectCatalogueViewShowAnswers: Bool {
     get {return S.get(true, #keyPath(subjectCatalogueViewShowAnswers))}
-    set(n) {S.set(#keyPath(subjectCatalogueViewShowAnswers), n)}
+    set(n) {S.set(n, #keyPath(subjectCatalogueViewShowAnswers))}
   }
 }
